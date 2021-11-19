443efba6b1722636bf40aff567d4a1e4e9805ab9
José Malcher Jr <contato@josemalcher.net>
2021-11-19 16:13:27 -0300

    06 Ponto único de entrada

---
+++ gerenciador-cursos/public/index.php
@@ -0,0 +1,13 @@
+<?php
+switch ($_SERVER['PATH_INFO']) {
+    case '/listar-cursos':
+        require 'listar-cursos.php';
+        break;
+    case '/novo-curso':
+        require 'formulario-novo-curso.php';
+        break;
+    default:
+        echo "Erro 404";
+        break;
+
+}
